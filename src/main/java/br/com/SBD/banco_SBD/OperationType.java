6e69e12f4781f163234ac1877b04792cf965ae56
Rodrigo Martins <rodrigocomputadores125@hotmail.com>
2019-11-19 18:57:25 -0300

    Add byId() and getId() methods to OperationType

--- src/main/java/br/com/SBD/banco_SBD/OperationType.java
+++ src/main/java/br/com/SBD/banco_SBD/OperationType.java
@@ -1,6 +1,32 @@
 package br.com.SBD.banco_SBD;
 
-public enum OperationType
-{
-	DEPOSIT, WITHDRAW, TRANSFERENCE, OPEN_ACCOUNT;
+import java.util.Map;
+import java.util.HashMap;
+
+public enum OperationType {
+	DEPOSIT(0), WITHDRAW(1), TRANSFERENCE(2), ACCOUNT_OPEN(3), ACCOUNT_CLOSE(4);
+
+	private Integer id;
+
+	private OperationType(Integer id)
+	{
+		this.id = id;
+	}
+
+	private static final Map<Integer, OperationType> idMap = new HashMap<>();
+
+	static {
+		for (OperationType e : OperationType.values())
+			idMap.put(e.getId(), e);
+	}
+
+	public static OperationType byId(Integer id)
+	{
+		return idMap.get(id);
+	}
+
+	public final int getId()
+	{
+		return id;
+	}
 }
